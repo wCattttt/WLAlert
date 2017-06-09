94f1ac1ff0f20258742933d090cc48987d0a66c5
weiweilong <weiweilong_email@foxmail.com>
2017-06-09 09:41:27 +0800

    Initial Commit

---
+++ WLAlert/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WLAlert
+//
+//  Created by 魏唯隆 on 2017/6/9.
+//  Copyright © 2017年 魏唯隆. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
